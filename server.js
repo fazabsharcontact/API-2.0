58ef7a42cdea50dbd76cef666f0f130a899da19b
fazaabsharcontact <fazaabsharcontact@gmail.com>
2026-08-18 05:55:40 +0700

    create route route new and edit in server.js

--- server.js
+++ server.js
@@ -23,7 +23,23 @@ app.get("/", async (req, res) => {
 });
 
 //Route to render the edit page
+app.get("/new", (req, res) => {
+    res.render("modify.ejs", { heading: "New Post", submit: "Create Post "});
+});
 
+app.get("/edit/:id", async (req, res) => {
+    try {
+        const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+        console.log(response.data);
+        res.render("modify.ejs", {
+            heading: "Edit Post",
+            submit: "Update Post",
+            post: response.data,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching post "});
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}....`);
