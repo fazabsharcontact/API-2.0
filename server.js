63a5bb49d6b016b338871dc455cb95f6d8bd0059
fazaabsharcontact <fazaabsharcontact@gmail.com>
2026-08-19 21:48:13 +0700

    adding route new post, update post, and delete post in server.js

--- server.js
+++ server.js
@@ -41,6 +41,42 @@ app.get("/edit/:id", async (req, res) => {
     }
 });
 
+//Create a new post
+app.post("/api/posts", async (req, res) => {
+    try {
+        const response = await axios.poist(`${API_URL}/posts`, req.body);
+        console.log(response.data);
+        res.redirect("/");
+    } catch (error) {
+        res.status(500).json({ message: "Error creating post" });
+    }
+});
+
+//Partially update a post
+app.post("/api/posts/:id", (req, res) => {
+    console.log("called");
+    try {
+        const response = await axios.patch(
+            `${API_URL}/posts/${req.params.id}`,
+            req.body
+        );
+        console.log(response.data);
+        res.redirect("/");
+    } catch (error) {
+        res.status(500).json({ message: "Error updating post" });
+    }
+});
+
+//Delete a post
+app.get("/api/posts/delete/:id", async (req, res) => {
+    try {
+        await axios.delete(`${API_URL}/posts/${req.params.id}`);
+        res.redirect("/");
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting post" });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}....`);
 });
